574a3cfc0a85df4bfd438d71da66e5e14f8d3671
Paul Newland <paulnewland@Pauls-MacBook-Air.local>
2017-06-15 20:18:18 -0700

    view tool initial commit

---
+++ lib/newland_view_tool.rb
@@ -0,0 +1,6 @@
+require "newland_view_tool/version"
+require "newland_view_tool/renderer"
+
+module NewlandViewTool
+  # Your code goes here...
+end
---
+++ lib/newland_view_tool/version.rb
@@ -0,0 +1,3 @@
+module NewlandViewTool
+  VERSION = "0.1.0"
+end
